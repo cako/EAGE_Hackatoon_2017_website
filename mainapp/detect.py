42eb54df76cee70fdb3cab80085a6f450dabf15e
cdacosta <c.dacostaf@gmail.com>
2017-06-11 14:20:23 +0200

    working

---
+++ mainapp/detect.py
@@ -0,0 +1,77 @@
+import cv2
+from django.conf import settings
+
+def detectfaces(path):
+
+	img = cv2.imread(settings.BASE_DIR+path)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	path_cascade  = settings.BASE_DIR+'/mainapp/static/mainapp/assets/haarcascades/'
+	face_cascade = cv2.CascadeClassifier(path_cascade+'haarcascade_frontalface_default.xml')
+
+	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
+
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+	fname = path.split('.')
+	filename = ('.').join(fname[:-1])
+	returnpath = settings.BASE_DIR + filename + "_detected.jpg" 
+
+	cv2.imwrite(returnpath, img)
+
+	return returnpath
+
+def detectfeatures(path,cascades,scale_fact=1.2,nbrs=3,minsize=00,maxsize=100):
+
+	img = cv2.imread(settings.BASE_DIR+path)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	height, width, channels = img.shape
+	mi = minsize * min(height,width) / 100
+	ma = max(maxsize * min(height, width) / 100, mi)
+
+	path_cascade  = settings.BASE_DIR+'/mainapp/static/mainapp/assets/haarcascades/'
+	font = cv2.FONT_HERSHEY_DUPLEX
+	
+	if cascades['face'] == True:
+		face_cascade = cv2.CascadeClassifier(path_cascade+'haarcascade_frontalface_default.xml')
+		faces = face_cascade.detectMultiScale(gray,scaleFactor=scale_fact,minNeighbors=nbrs,maxSize=(ma,ma),minSize=(mi,mi))
+		for (x,y,w,h) in faces:
+			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),6)
+			cv2.rectangle(img, (x+ w/2, y + h/2), (x + w/2+1, y + h/2 + 1), (255, 0, 0), 6)
+			cv2.putText(img,'face',(x,y),font,2,(255,0,0),2)
+
+	if cascades['bottle'] == True:
+		bottle_cascade = cv2.CascadeClassifier(path_cascade+'bottle_7stagecascade.xml')
+		bottles = bottle_cascade.detectMultiScale(gray,scaleFactor=scale_fact,minNeighbors=nbrs,maxSize=(ma,ma),minSize=(mi,mi))
+		for (x,y,w,h) in bottles:
+			cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),6)
+			cv2.rectangle(img, (x+ w/2, y + h/2), (x + w/2+1, y + h/2 + 1), (0, 255, 0), 6)
+			cv2.putText(img,'bottle',(x,y),font,2,(0,255,0),2)
+
+	if cascades['fault'] == True:
+		fault_cascade = cv2.CascadeClassifier(path_cascade+'fault_cascades.xml')
+		faults = fault_cascade.detectMultiScale(gray,scaleFactor=scale_fact,minNeighbors=nbrs,maxSize=(ma,ma),minSize=(mi,mi))
+		for (x,y,w,h) in faults:
+			cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),6)
+			cv2.rectangle(img, (x+ w/2, y + h/2), (x + w/2+1, y + h/2 + 1), (0, 0, 255), 6)
+			cv2.putText(img,'fault',(x,y),font,2,(0,0,255),2)
+
+	if cascades['trap'] == True:
+		trap_cascade = cv2.CascadeClassifier(path_cascade+'trap_3stagecascade.xml')
+		traps = trap_cascade.detectMultiScale(gray,scaleFactor=scale_fact,minNeighbors=nbrs,maxSize=(ma,ma),minSize=(mi,mi))
+		for (x,y,w,h) in traps:
+			cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),6)
+			cv2.rectangle(img, (x+ w/2, y + h/2), (x + w/2+1, y + h/2 + 1), (255, 255, 255), 6)
+			cv2.putText(img,'trap',(x,y),font,2,(255,255,255),2)
+
+	fname = path.split('.')
+	ext = fname[-1]
+	filename = ('.').join(fname[:-1])
+	returnpath = settings.BASE_DIR + filename + "_detected." + ext 
+
+	cv2.imwrite(returnpath, img)
+
+	return returnpath
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,7 +4,12 @@ from django.core.files.storage import FileSystemStorage
 from django.utils.text import slugify
 
 import os, glob
-from detectfaces import detectfaces
+from detect import detectfaces, detectfeatures
+
+import decimal
+
+uploaded_file_url = ''
+current_image = 'pic01.jpg'
 
 def get_images_list(directory):
     files = glob.glob(directory + '/*.jpg')
@@ -17,12 +22,24 @@ class IndexView(TemplateView):
     template_name = 'index.html'
     title = "It's not our FAULT!"
 
+    casc_dict = {'fault': False, 'trap': False, 'face': False, 'bottle': False}
+    pars_dict = {'neighbors': 3, 'scale': 1.2, 'min': 20, 'max': 80}
+    uploaded_file_url = ''
+    current_image = 'pic01.jpg'
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['title'] = self.title
 
+        for key, value in self.casc_dict.iteritems():
+            context[key] = value
+        for key, value in self.pars_dict.iteritems():
+            context[key] = value
         context['images_loc'] = FileSystemStorage().location
         context['images_list'] = get_images_list(context['images_loc'])
+        context['current_image'] = current_image
+        print 'GET ' + current_image
+        print 'GET ' + uploaded_file_url
         return context
 
     def post(self, request, *args, **kwargs):
@@ -36,11 +53,45 @@ class IndexView(TemplateView):
             fname = slugify('_'.join(fname_ext[:-1])) + '.' + ext
             filename = fs.save(fname, myfile)
             uploaded_file_url = fs.url(filename)
-            context['uploaded_file_url'] = uploaded_file_url
             context['images_loc'] = fs.location
             context['images_list'] = get_images_list(context['images_loc'])
 
-            result_file_url = detectfaces(uploaded_file_url)
-            context['result_file_url'] = result_file_url
+        global uploaded_file_url
+        global current_image
+        context['uploaded_file_url'] = uploaded_file_url
 
+        cascades = ['fault', 'trap', 'face', 'bottle']
+        for c in cascades:
+            if request.POST.get(c):
+                self.casc_dict[c] = True
+            else:
+                self.casc_dict[c] = False
+
+        if 'neighbors' in request.POST:
+            self.pars_dict['neighbors'] = int(request.POST['neighbors'])
+        if 'scale' in request.POST:
+            self.pars_dict['scale'] = float(request.POST['scale'])
+        if 'min' in request.POST:
+            self.pars_dict['min'] = int(request.POST['min'])
+        if 'max' in request.POST:
+            self.pars_dict['max'] = int(request.POST['max'])
+
+        if uploaded_file_url:
+            print(self.casc_dict)
+            result_file_url = detectfeatures(context['uploaded_file_url'], self.casc_dict, scale_fact=self.pars_dict['scale'],
+                    nbrs=self.pars_dict['neighbors'], minsize=self.pars_dict['min'], maxsize=self.pars_dict['max'])
+            context['result_file_url'] = result_file_url
+            current_image = os.path.basename(result_file_url)
+            context['current_image'] = current_image
+	
+        print context['current_image']
+
+        for key, value in self.casc_dict.iteritems():
+            context[key] = value
+        for key, value in self.pars_dict.iteritems():
+            context[key] = value
+
+        print request.POST
+        print 'POST ' + current_image
+        print 'POST ' + uploaded_file_url
         return super(TemplateView, self).render_to_response(context)
